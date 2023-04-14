62ef6623f9df97176384753743515c55fc2db0cb
Sanchez Villalobos <cesarasa@ttu.edu>
2023-04-14 17:28:00 -0500

    Dumb minor things checked, some extra comments related to m.

--- code/rba.py
+++ code/rba.py
@@ -7,6 +7,16 @@ class relief(object):
     be used for learning and documentation purposes. The main goal of the code is not to
     replace the previous implementations, but provide a better understanding of the 
     algorithm. 
+
+    n_features_to_keep: Number of features to keep. (self explanatory, tbh)
+    X: The feature matrix, this shall be a numpy array (NOTE: add an assertion for this, and if not, convert it to a numpy array).
+    y: The target vector, this shall be a numpy array (NOTE: add an assertion for this, and if not, convert it to a numpy array).
+
+    Note that, just as skrebate, we won't use the m parameter, as the scores are more representative is m=N, so we will use the whole 
+    dataset to compute the scores, always. 
+
+    NOTE: we should be using the distance metric from the paper, but I guess the norm will do it for now.
+    NOTE 2: Are there an explanation of why the distance is euclidean or so? Which is the best distance? How the metric affects the results?
     """
     def __init__(self, n_features_to_keep=2):
         self.n_features_to_keep = n_features_to_keep
@@ -23,14 +33,13 @@ class relief(object):
         self.weights = np.zeros(self.D)
         for i in range(self.N):
             # Get the current instance: 
-            x_i = self.X[i]
+            x_i = self.X[i, :]
             y_i = self.y[i]
             # Get the nearest hit and miss: 
             hit = self.get_nearest_hits(x_i, y_i)
             miss = self.get_nearest_miss(x_i, y_i)
             # Update the weights: 
-            self.weights -= (x_i - hit)**2 - (x_i - miss)**2
-            print(self.weights)
+            self.weights -= (x_i - hit)**2/self.N - (x_i - miss)**2./self.N
         # Get the indices of the features to keep:
         self.indices = np.argsort(self.weights)[::-1][:self.n_features_to_keep]
         # Resulting dataset is:
@@ -40,7 +49,7 @@ class relief(object):
         # Get the nearest hits: 
         hits = self.X[self.y == y_i]
         # Get the distance to the current instance: 
-        distances = np.linalg.norm(hits - x_i, axis=1)
+        distances = np.linalg.norm(hits - x_i, axis=1, ord=1)
         # Get the nearest hit: 
         nearest_hit = hits[np.argmin(distances)]
         return nearest_hit
@@ -49,8 +58,32 @@ class relief(object):
         # Get the nearest miss: 
         miss = self.X[self.y != y_i]
         # Get the distance to the current instance: 
-        distances = np.linalg.norm(miss - x_i, axis=1)
+        distances = np.linalg.norm(miss - x_i, axis=1, ord=1)
         # Get the nearest miss: 
         nearest_miss = miss[np.argmin(distances)]
         return nearest_miss
+
+class relief_f(object):
+    """author: cesar augusto sanchez-villalobos @cesarasa.
+    
+    The following code is a python implementation of the relief-F algorithm, which is an 
+    extension of the relief algorithm. 
+
+    The main difference, is that relief-F uses more than one nearest hit and miss to
+    compute the weights.
+
+    n_features_to_keep: Number of features to keep. (self explanatory, tbh)
+    X: The feature matrix, this shall be a numpy array (NOTE: add an assertion for this, and if not, convert it to a numpy array).
+    y: The target vector, this shall be a numpy array (NOTE: add an assertion for this, and if not, convert it to a numpy array).
+    k: Number of nearest hits and misses to use.
+
+    
+    """
+    def __init__(self, n_features_to_keep=2, k=5):
+        self.n_features_to_keep = n_features_to_keep
+        self.k = k
+
+    def fit(self, X, y):
+        pass
+    
 
--- test_dr_pal_example.py
+++ test_dr_pal_example.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from code.rba import relief
+from code.rba import relief, relief_f
 
 # Create a synthetic dataset with two classes and 10 features
 X = np.array([[1, 2],
@@ -10,10 +10,12 @@ X = np.array([[1, 2],
              [2, 5],
              [2, 6]])
 
+# Let's add some noisy features:
+X = np.hstack((X, np.random.rand(6, 1)))
 y = np.array([1, 1, 1, 0, 0, 0])
 
 # Create the relief object
-r = relief(n_features_to_keep=1)
+r = relief(n_features_to_keep=2)
 
 # Fit the model
 r.fit(X, y)
@@ -23,3 +25,31 @@ X_new = r.X_new
 
 # Print it:
 print(X_new)
+
+# Plot importance of features
+plt.figure()
+plt.bar(range(len(r.weights)), r.weights, color='peru', ec='k', lw=2, align='center')
+plt.title('Feature importance')
+plt.xlabel('Feature')
+plt.ylabel('Importance')
+plt.show()
+
+# Create the relief object
+r = relief_f(n_features_to_keep=2, k = 3)
+
+# Fit the model
+r.fit(X, y)
+
+# Get the resulting dataset
+X_new = r.X_new
+
+# Print it:
+print(X_new)
+
+# Plot importance of features
+plt.figure()
+plt.bar(range(len(r.weights)), r.weights, color='peru', ec='k', lw=2, align='center')
+plt.title('Feature importance')
+plt.xlabel('Feature')
+plt.ylabel('Importance')
+plt.show()
